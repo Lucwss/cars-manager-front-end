82e2122f407ffe1e2a59d783ac328bbe2562aa97
lucas emanoel <lucws2002@gmail.com>
2023-03-19 14:46:40 -0400

    pages car, new car, cars

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
+import {Link} from "react-router-dom"
+
 const Card = ({ car }) => {
-    let { brand, price, model, year, color } = car
+    let { brand, price, make, year, km, cm3, _id } = car
     return (
         <div className="shadow-md p-5 flex flex-col">
             <div className="font-extrabold text-center border-b-2">{brand} {model}</div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Car from './pages/Car';
+import Cars from './pages/Cars';
+import NewCar from './pages/NewCar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App/>} />
+            <Route path='cars' element={<Cars />} />
+            <Route path='cars/new' element={<NewCar />} />
+            <Route path='cars/:id' element={<Car />} />
+            {/* <Route path='about' element={<About />} /> */}
+        </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>
 );
